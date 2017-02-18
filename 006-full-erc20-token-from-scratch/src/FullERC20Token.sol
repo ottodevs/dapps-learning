ab92961e8106b45bffb101ede4e79aca9b2acb98
reverendus <reverendus@invalid>
2017-02-18 20:01:48 +0100

    Starting to build a full ERC20 token from scratch
    An empty implementation and a set of tests courtesy of DAppHub.
    Will build a full implementation fo an ERC20 token on it.

---
+++ 006-full-erc20-token-from-scratch/src/FullERC20Token.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.9;
+
+import './erc20/src/erc20.sol';
+
+contract FullERC20Token is ERC20 {
+    function FullERC20Token(uint256 initialSupply) {
+    }
+}
---
+++ 006-full-erc20-token-from-scratch/src/FullERC20TokenTest.sol
@@ -0,0 +1,137 @@
+/*
+   Copyright 2016 Nexus Development, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+pragma solidity ^0.4.8;
+
+import "ds-test/src/test.sol";
+
+import "./FullERC20Token.sol";
+
+contract TokenUser {
+    ERC20  token;
+
+    function TokenUser(ERC20 token_) {
+        token = token_;
+    }
+
+    function doTransferFrom(address from, address to, uint amount)
+        returns (bool)
+    {
+        return token.transferFrom(from, to, amount);
+    }
+
+    function doTransfer(address to, uint amount)
+        returns (bool)
+    {
+        return token.transfer(to, amount);
+    }
+
+    function doApprove(address recipient, uint amount)
+        returns (bool)
+    {
+        return token.approve(recipient, amount);
+    }
+
+    function doAllowance(address owner, address spender)
+        constant returns (uint)
+    {
+        return token.allowance(owner, spender);
+    }
+
+    function doBalanceOf(address who) constant returns (uint) {
+        return token.balanceOf(who);
+    }
+}
+
+contract FullERC20TokenTest is DSTest {
+    uint constant initialBalance = 1000;
+
+    ERC20 token;
+    TokenUser user1;
+    TokenUser user2;
+
+    function DSTokenTest() {
+        token = createToken();
+        user1 = new TokenUser(token);
+        user2 = new TokenUser(token);
+    }
+
+    function createToken() internal returns (ERC20) {
+        return new FullERC20Token(initialBalance);
+    }
+
+    function testSetupPrecondition() {
+        assertEq(token.balanceOf(this), initialBalance);
+    }
+
+    function testTransferCost() logs_gas() {
+        token.transfer(address(0), 10);
+    }
+
+    function testAllowanceStartsAtZero() logs_gas {
+        assertEq(token.allowance(user1, user2), 0);
+    }
+
+    function testValidTransfers() logs_gas {
+        uint sentAmount = 250;
+        log_named_address("token11111", token);
+        token.transfer(user2, sentAmount);
+        assertEq(token.balanceOf(user2), sentAmount);
+        assertEq(token.balanceOf(this), initialBalance - sentAmount);
+    }
+
+    function testFailWrongAccountTransfers() logs_gas {
+        uint sentAmount = 250;
+        token.transferFrom(user2, this, sentAmount);
+    }
+
+    function testFailInsufficientFundsTransfers() logs_gas {
+        uint sentAmount = 250;
+        token.transfer(user1, initialBalance - sentAmount);
+        token.transfer(user2, sentAmount+1);
+    }
+
+
+    function testApproveSetsAllowance() logs_gas {
+        log_named_address("Test", this);
+        log_named_address("Token", token);
+        log_named_address("Me", this);
+        log_named_address("User 2", user2);
+        token.approve(user2, 25);
+        assertEq(token.allowance(this, user2), 25);
+    }
+
+    function testChargesAmountApproved() logs_gas {
+        uint amountApproved = 20;
+        token.approve(user2, amountApproved);
+        assert(user2.doTransferFrom(this, user2, amountApproved));
+        assertEq(token.balanceOf(this), initialBalance - amountApproved);
+    }
+
+    function testFailTransferWithoutApproval() logs_gas {
+        address self = this;
+        token.transfer(user1, 50);
+        token.transferFrom(user1, self, 1);
+    }
+
+    function testFailChargeMoreThanApproved() logs_gas {
+        address self = this;
+        token.transfer(user1, 50);
+        user1.doApprove(self, 20);
+        token.transferFrom(user1, self, 21);
+    }
+}
+
