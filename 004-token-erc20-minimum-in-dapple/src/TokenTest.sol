5a05c185f4603624e21be2c9ed3360802503b614
reverendus <reverendus@invalid>
2017-02-18 18:21:25 +0100

    004-token-erc20-minimum-in-dapple unit tests in dapple added

--- 004-token-erc20-minimum-in-dapple/src/TokenTest.sol
+++ 004-token-erc20-minimum-in-dapple/src/TokenTest.sol
@@ -4,13 +4,73 @@ import 'dapple/test.sol';
 import './Token.sol';
 
 contract TokenTest is Test {
+    Token token = new Token(100, "Test Token", 16, "TTK");
+    Tester user1;
+    Tester user2;
 
-    function testNameSymbolsAndDecimalsPreserved() {
-        Token token = new Token(100, "Test Token", 16, "TTK");
-        token.transfer( address(this), 50 );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-        string name = token.name;
-        assertEq( name, "Test Token" );
-        assertEq( token.decimals, 16 );
+    function setUp() {
+        user1 = new Tester();
+        user1._target(token);
+
+        user2 = new Tester();
+        user2._target(token);
     }
+
+    function testDecimalsArePreserved() {
+        assertTrue( token.decimals() == uint8(16) );
+    }
+
+    function testInitialSupplyBelongsToTheCreator() {
+        assertEq( token.balanceOf(address(this)), 100 );
+    }
+
+    function testBalanceOfOtherAddressesIsEmpty() {
+        assertEq( token.balanceOf(address(user1)), 0 );
+        assertEq( token.balanceOf(address(user2)), 0 );
+    }
+
+    function testPartialTransferToSomeOtherAddress() {
+        token.transfer( address(user1), 40 );
+
+        assertEq( token.balanceOf(address(this)), 60 );
+        assertEq( token.balanceOf(address(user1)), 40 );
+    }
+
+    function testCompleteTransferToSomeOtherAddress() {
+        token.transfer( address(user1), 100 );
+
+        assertEq( token.balanceOf(address(this)), 0 );
+        assertEq( token.balanceOf(address(user1)), 100 );
+    }
+
+    function testManyTransfersBetweenAddresses() {
+        // this -> user1
+        token.transfer( address(user1), 10 );
+        token.transfer( address(user1), 30 );
+        
+        // user1 -> user2
+        Token(user1).transfer( address(user2), 15 );
+        
+        // user2 -> this
+        Token(user2).transfer( address(this), 7 );
+
+        assertEq( token.balanceOf(address(this)), 67 );
+        assertEq( token.balanceOf(address(user1)), 25 );
+        assertEq( token.balanceOf(address(user2)), 8 );
+    }
+
+    function testEmittingTheTransferEvent() {
+        expectEventsExact( token );
+        Transfer( address(this), address(user1), 35 );
+
+        token.transfer( address(user1), 35 );
+    }
+
+    function testFailsToSendMoreThanTheBalance() {
+        token.transfer( address(user1), 101 );
+    }
+
+    // would be nice to have a test for overflow as well
 }
