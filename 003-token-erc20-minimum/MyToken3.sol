99bbc459bac44221acac8b19c5cfcaf6800f0ccd
reverendus <reverendus@invalid>
2017-02-18 12:35:53 +0100

    003-token-erc20-minimum/MyToken3 added
    First ERC20-compatible token.

---
+++ 003-token-erc20-minimum/MyToken3.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.8;
+
+contract MyToken3 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    mapping (address => uint256) public balanceOf;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    function MyToken3(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+    
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        Transfer(msg.sender, _to, _value);
+    }
+}
+
