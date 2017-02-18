baf042dee12640a70eaa8e3c94d76a0d70bb7650
reverendus <reverendus@invalid>
2017-02-18 19:15:08 +0100

    Minimal ERC20 token build using truffle

---
+++ 005-token-erc20-minimum-in-truffle/contracts/Token.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.8;
+
+import './TokenEventDefinitions.sol';
+
+contract Token is TokenEventDefinitions {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    mapping (address => uint256) public balanceOf;
+    
+    function Token(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+    
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        Transfer(msg.sender, _to, _value);
+    }
+}
+
---
+++ 005-token-erc20-minimum-in-truffle/contracts/TokenEventDefinitions.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.8;
+
+contract TokenEventDefinitions {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
